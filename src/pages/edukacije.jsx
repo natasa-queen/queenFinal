bffcd6e54b3645d2cd27f0304960e504fcef0b5b
A.M. <andrej2000aa@gmail.com>
2023-11-06 10:52:39 +0100

    sitemap and robots

--- src/pages/edukacije.jsx
+++ src/pages/edukacije.jsx
@@ -10,7 +10,49 @@ const Edukacije = () => {
           <div className="edukacije-wraper">
             <h2>Edukacije</h2>
 
-            <h4><i>Stranica u pripremi</i></h4>
+            <div className="edu-wrap">
+              <h5>Usavršite svoje vještine uz pomoć stručnih edukatora</h5>
+
+              <p>
+                Bez obzira na vaš trenutni nivo znanja i vještina, u našem salonu pronaći ćete edukaciju koja će Vam odgovarati.
+              </p>
+              <p>
+                U ponudi imamo edukacije za početnike i  napredne buduce sminkere i "nail strucnjake".
+                Edukacije vode iskusni i stručni edukatori, oni će vam pružiti sve potrebne informacije i vježbe kako biste usavršili svoje vještine.
+              </p>
+              <p>
+                Naše edukacije su dizajnirane tako da vam pruže praktično iskustvo. Imat ćete priliku vježbati svoje vještine na modelima pod nadzorom naših edukatora. Također, ćete dobiti individualnu pažnju i povratne informacije kako biste mogli napredovati.
+              </p>
+
+              <p>
+                Na kraju edukacije, svaki polaznik dobiva certifikat koji potvrđuje vaše stiecanje vještina i znanja. Ovaj certifikat priznaje vaš trud i označava kvalitetu edukacije koju ste prošli kod nas.
+              </p>
+
+              <h5>Sminkanje</h5>
+              <p>
+                Želite li naučiti kako profesionalno šminkati sebe ili druge? Trazite pouzdane edukatore koji će vam pružiti osnovna znanja i vještine iz svijeta sminkanja? Ako je tako, onda je kozmetički salon Queen idealno mjesto za vas!
+              </p>
+              <p>
+                Naše edukacije pokrivaju širok spektar tema, od osnova sminkanja do najnovijih trendova u toj oblasti.
+              </p>
+
+              <h5>Njega noktiju</h5>
+              <p>
+                Edukacija posvećena ljepoti noktiju i stručnosti. Naša strast je stvaranje remek-djela na vašim rukama, istovremeno oblikujući buduće stručnjake u području njege noktiju.
+              </p>
+              <p>
+                Naši kursevi za oblikovanje noktiju pružaju sveobuhvatno iskustvo. Bez obzira jeste li početnik ili iskusni tehničar, naš cilj je pružiti vam najnovije tehnike, trendove i praktično znanje kako biste postali stručnjak u ovom području.
+              </p>
+              <p>
+                Od osnova manikure do naprednih tehnika poput geliranja i nadogradnje noktiju, naši kursevi pokrivaju širok spektar vještina. Naučit ćete o pravilnoj pripremi noktiju, upotrebi različitih materijala i tehnika kako biste postigli nokte koji oduzimaju dah.
+              </p>
+              <br/>
+
+              <p>
+                <em>Za vise informacija slobodno nas kontaktirajte.</em>
+              </p>
+
+            </div>
           </div>
         </Layout>
     )
